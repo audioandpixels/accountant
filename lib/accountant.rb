ae716eff09a961ac1dc8933c7cc5f83890b51629
jasoncox <jason@audioandpixels.com>
2014-08-27 10:17:58 +1000

    Use money-rails and monetize

--- lib/accountant.rb
+++ lib/accountant.rb
@@ -1,8 +1,8 @@
 require 'active_record'
 require 'action_controller'
 require 'money-rails'
-# require 'money-rails/hooks'
-# MoneyRails::Hooks.init
+require 'money-rails/hooks'
+MoneyRails::Hooks.init
 
 require 'accountant/version'
 require 'accountant/transfer'
@@ -11,9 +11,9 @@ require 'accountant/journal'
 require 'accountant/line'
 require 'accountant/active_record/extensions'
 
-# ActiveRecord::Base.class_eval do
-#   include Accountant::ActiveRecordExtension
-# end
+ActiveRecord::Base.class_eval do
+  include Accountant::ActiveRecordExtension
+end
 
 require 'accountant/global_account'
 require 'accountant/manually_created_account'
